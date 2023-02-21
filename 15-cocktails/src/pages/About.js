5325d7f6ceeb8be38c453726ddd249f4acf4018b
nikkoberse <nikko.berse.23@gmail.com>
2023-02-21 16:10:23 +0800

    basic projects - cocktails

---
+++ 15-cocktails/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        A paragraph is defined as “a group of sentences or a single sentence
+        that forms a unit” (Lunsford and Connors 116). Length and appearance do
+        not determine whether a section in a paper is a paragraph. For instance,
+        in some styles of writing, particularly journalistic styles, a paragraph
+        can be just one sentence long.
+      </p>
+    </section>
+  )
+}
+
+export default About
