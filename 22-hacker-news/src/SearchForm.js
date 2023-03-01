d53430d1ffbbc0a422a4e416ebb4d2c6a0cf9b71
nikkoberse <nikko.berse.23@gmail.com>
2023-03-01 17:12:57 +0800

    Finished projects

---
+++ 22-hacker-news/src/SearchForm.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { useGlobalContext } from "./context"
+
+const SearchForm = () => {
+  const { searchStory, query } = useGlobalContext()
+
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>search hacker news</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => searchStory(e.target.value)}
+      />
+    </form>
+  )
+}
+
+export default SearchForm
